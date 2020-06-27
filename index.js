50af198e1690b8f78fc7e0e02c936ebf8f65b557
Brahim <brahim.akarouch@gmail.com>
2020-06-27 22:07:40 +0100

    add some comments

--- index.js
+++ index.js
@@ -10,62 +10,66 @@ async function runIt() {
   });
   const page = await browser.newPage();
 
+  // ! Import cookies
   const cookiesString = await fs.readFile("./cookies.json");
   const cookies = JSON.parse(cookiesString);
   await page.setCookie(...cookies);
   await page.setDefaultTimeout(0);
+
+  // ! Go to kdp dashboard
   await page.goto(
     // "https://kdp.amazon.com/en_US/title-setup/paperback/new/details?ref_=kdp_BS_D_cr_ti"
     "https://kdp.amazon.com/en_US/title-setup/paperback/new/details?ref_=kdp_BS_D_cr_ti"
   );
-  //   await page.waitFor(randomInt(1298, 8419));
   await page.waitFor(randomInt(1298, 1419));
-  await page.click("#data-print-book-title");
 
+  // ! Title
+  await page.click("#data-print-book-title");
   randomInt(1298, 2419);
-  await page.type("#data-print-book-title", "HY I'M TITLE", {
+  await page.type("#data-print-book-title", "Hi I'M TITLE", {
     delay: 120,
   });
 
+  // ! Subtitle
   randomInt(1298, 1419);
   await page.click("#data-print-book-subtitle");
-
   randomInt(298, 419);
-  await page.type("#data-print-book-subtitle", "HY I'M A SUBTITLE", {
+  await page.type("#data-print-book-subtitle", "Hi I'M A SUBTITLE", {
     delay: 120,
   });
 
+  // ! First name
   randomInt(298, 519);
   await page.click("#data-print-book-primary-author-first-name");
-
   randomInt(28, 191);
   await page.type(
     "#data-print-book-primary-author-first-name",
-    "HY I'M A FIRST NAME",
-    {
-      delay: 120,
-    }
+    "Hi I'M A FIRST NAME",
+    { delay: 120 }
   );
 
+  // ! Last name
   randomInt(298, 419);
   await page.click("#data-print-book-primary-author-last-name");
 
   randomInt(298, 819);
   await page.type(
     "#data-print-book-primary-author-last-name",
-    "HY I'M A SECOND NAME",
+    "HY I'M A LAST NAME",
     {
       delay: 120,
     }
   );
+
+  // ! Description
   randomInt(118, 719);
   await page.click("#data-print-book-description");
-
   randomInt(192, 619);
-  await page.type("#data-print-book-description", "HY I'M A DESCRIPTION", {
+  await page.type("#data-print-book-description", "Hi I'M A DESCRIPTION", {
     delay: 120,
   });
 
+  // ! Select no public domain
   await page.waitFor(randomInt(1298, 8419));
   await page.click("#non-public-domain", {
     delay: 25,
@@ -123,34 +127,25 @@ async function runIt() {
   // ! Categories
   await page.click("#data-print-book-categories-button-proto-announce");
   await page.waitFor(5000);
-  //   const textsArray = await page.evaluate(() =>
-  //     [
-  //       ...document.querySelectorAll("#category-chooser-popover .a-link-normal"),
-  //     ].map((elem) => elem.click())
-  //   );
-  //   await page.evaluate(() => {
-  //     document
-  //       .querySelectorAll("#category-chooser-popover .a-link-normal")[1]
-  //       .click();
-  //   });
-
   await page.waitFor(randomInt(5000, 5222));
-  const clickThemAll = await page.$$eval(
-    "#category-chooser-popover .a-link-normal",
-    (links) => links.forEach((link) => link.click())
+
+  // ! Expand all links
+  await page.$$eval("#category-chooser-popover .a-link-normal", (links) =>
+    links.forEach((link) => link.click())
   );
   await page.waitFor(2000);
   await page.click("#data-print-book-categories-button-proto-announce");
   await page.click("#unsaved-changes-cancel-announce");
-  //   await page.waitFor(25000);
   await page.waitFor(25000);
 
-  const clickSeleted = await page.$$eval(".a-label", (links) =>
+  // ! Select the excact categories
+  await page.$$eval(".a-label", (links) =>
     links.forEach((el) => {
       el.textContent === "Global Warming & Climate Change" ? el.click() : true;
       el.textContent === "Meteorology & Climatology" ? el.click() : true;
     })
   );
+  // * Click save
   await page.waitFor(5000);
   await page.$$eval(".a-button-input", (elements) => elements[4].click());
 
@@ -160,49 +155,41 @@ async function runIt() {
     elements[0].click()
   );
 
-  // ! Save And Continue
+  // ! Save first page and continue
   await page.waitFor(randomInt(2000, 3000));
   await page.click("#save-and-continue-announce");
 
   // ! Get Free ISBN
-
   await page
     .waitForFunction(
       "document.querySelector('#free-print-isbn-btn-announce') && document.querySelector('#free-print-isbn-btn-announce').clientHeight != 0"
     )
     .then(() => console.log("got it!"));
-
-  // ! another solution (i didnt use)
-  // page.waitForSelector("#free-print-isbn-btn-announce", { visible: true });
-
-  //
   await page.click("#free-print-isbn-btn-announce");
   await page.waitFor(randomInt(1000, 2000));
   await page.click("#print-isbn-confirm-button-announce");
   await page.waitFor(randomInt(4000, 5000));
 
-  // ! Upload Inside
+  // ! Upload Interior
   const inputUploadHandle = await page.$(
     "#data-print-book-publisher-interior-file-upload-AjaxInput"
   );
   let fileToUpload = "./pdf/inside.pdf";
-
   inputUploadHandle.uploadFile(fileToUpload);
 
+  // ! Check if interior uploading is started
   await page
     .waitForXPath("//*[@class='success-header' and contains(., 'inside.pdf')]")
     .then((res) => console.log("Upload your interior process is started ✅"));
 
-  // ! await for selector : NOT WORKING
-
-  // ! await for XPath :
-  // ! IMPORTANT data[print_book][publisher_interior][status]
+  // ! Check if interior processing is started
   await page
     .waitForXPath(
       "//*[@id='data-print-book-publisher-interior-file-upload-success' and contains(., 'Processing your file...')]"
     )
     .then((res) => console.log("Processing your interior is started... ✅"));
 
+  // ! Check if interior processing is finished
   await page
     .waitForFunction(
       () =>
@@ -225,11 +212,15 @@ async function runIt() {
   );
   let coverFile = "./pdf/cover.pdf";
   CoverUploadHandle.uploadFile(coverFile);
+
+  // ! Check if interior processing is started
   await page
     .waitForXPath(
       "//*[@id='data-print-book-publisher-cover-file-upload-success' and contains(., 'Processing your file...')]"
     )
     .then((res) => console.log("Processing your cover is started... ✅"));
+
+  // ! Check if interior processing is finished
   await page
     .waitForFunction(
       () =>
@@ -255,7 +246,7 @@ async function runIt() {
     .click(" #printpreview_approve_button_enabled a")
     .then((res) => console.log("Click Save"));
 
-  // ! go next page
+  // ! Go to third page
   await page
     .waitForFunction(
       "document.querySelector('#save-and-continue-announce') && document.querySelector('#save-and-continue-announce').clientHeight != 0"
@@ -266,14 +257,12 @@ async function runIt() {
     .click("#save-and-continue-announce")
     .then((res) => console.log("Going to third page "));
 
-  // ! Type a price
-
+  // ! Type a Price
   await page
     .waitForFunction(
       "document.querySelector('#data-pricing-print-us-price-input > input') && document.querySelector('#data-pricing-print-us-price-input > input').clientHeight != 0"
     )
     .then(() => console.log("got it 3!"));
-
   await page.evaluate(
     () =>
       (document.querySelector(
@@ -288,6 +277,7 @@ async function runIt() {
   // ! Click Expanded Distribution
   await page.click("#data-pricing-print label > i");
 
+  // ! Click save to draft
   await page.waitFor(2000);
   await page.click("#save-announce");
 
